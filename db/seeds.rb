443f389a2ca015c15e1f1a5ec08e50ff25952e95
TANIBUCHIHIROKAZU <tanibuchi1121@gmail.com>
2021-07-28 10:34:37 +0000

    add_env2

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,6 +6,6 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 Admin.create!(
-   email: ENV[SEED_ADMIN_EMAIL],
-   password: ENV[SEED_ADMIN_PASSWORD]
+   email: <%= ENV['SEED_ADMIN_EMAIL'] %>,
+   password: <%= ENV['SEED_ADMIN_PASSWORD'] %>
 )
